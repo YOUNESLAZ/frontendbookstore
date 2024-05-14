c5e4a8ba54efa7468ab8cc80577aa1f9185d81d3
YOUNESLAZ <144383815+YOUNESLAZ@users.noreply.github.com>
2024-05-14 20:30:54 +0100

    fronten

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import axios from "axios";
-import Book from "../../../Backend/model/book.model";
 
 function Cards({ item, onDelete }) {
   const handleDelete = async () => {
